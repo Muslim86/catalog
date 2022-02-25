4d019c409d3ad3064bdecf4b4f34cdc8f9a21e17
Muslim Temirkhanov <muslim.temirhanov_86@mail.ru>
2022-02-25 16:27:23 +0500

    add some tests

--- src/components/CatalogList/tests/CatalogList.test.tsx
+++ src/components/CatalogList/tests/CatalogList.test.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import CatalogList from '../CatalogList';
+import catalogListStore from '../../../stores/catalogListStore';
 
 const testId = 'testingCatalogList';
 
 type CatalogListProps = React.ComponentProps<typeof CatalogList>;
 
-const renderComponent = (props: CatalogListProps = {}) => render(
+const renderComponent = (props: CatalogListProps = { store: catalogListStore }) => render(
   <CatalogList
     data-testid={testId}
     {...props}
@@ -17,5 +19,22 @@ const renderComponent = (props: CatalogListProps = {}) => render(
 describe('Компонент CatalogList', () => {
   it('отображается без ошибок', () => {
     expect(renderComponent).not.toThrow();
+  });
+
+  it('should have show more', () => {
+    renderComponent();
+    expect(screen.getByText('Показать ещё')).toBeInTheDocument();
+  });
+
+  it('should hidden show more with type > 3', () => {
+    renderComponent();
+    userEvent.type(screen.getByRole('textbox'), 'React');
+    expect(screen.queryByText('Показать ещё')).not.toBeInTheDocument();
+  });
+
+  it('should hidden show more with type <= 3', () => {
+    renderComponent();
+    userEvent.type(screen.getByRole('textbox'), 'Rea');
+    expect(screen.queryByText('Показать ещё')).toBeInTheDocument();
   });
 });
--- src/components/CatalogVisibilityButton/tests/CatalogVisibilityButton.test.tsx
+++ src/components/CatalogVisibilityButton/tests/CatalogVisibilityButton.test.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import VisibilityButton from '../VisibilityButton';
+import catalogListStore from '../../../stores/catalogListStore';
 
 const testId = 'testingCatalogVisibilityButton';
 
 type CatalogVisibilityButtonProps = React.ComponentProps<typeof VisibilityButton>;
 
-const renderComponent = (props: CatalogVisibilityButtonProps = {}) => render(
+const renderComponent = (props: CatalogVisibilityButtonProps = { store: catalogListStore }) => render(
   <VisibilityButton
     data-testid={testId}
     {...props}
@@ -17,5 +19,16 @@ const renderComponent = (props: CatalogVisibilityButtonProps = {}) => render(
 describe('Компонент VisibilityButton', () => {
   it('отображается без ошибок', () => {
     expect(renderComponent).not.toThrow();
+  });
+
+  it('should be init on visible', () => {
+    renderComponent();
+    expect(screen.getByText('Скрыть')).toBeInTheDocument();
+  });
+
+  it('onClick handler work', () => {
+    renderComponent();
+    userEvent.click(screen.getByRole('button'));
+    expect(screen.getByText('Показать')).toBeInTheDocument();
   });
 });
